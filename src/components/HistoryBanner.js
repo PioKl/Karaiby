46e396357ad4d7ecba0fdadaa900c9c38e2b3a9f
PioKl <kosiu342@gmail.com>
2021-03-12 20:28:56 +0100

    HistoryBanner.js|Add new component

---
+++ src/components/HistoryBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/HistoryBanner.scss';
+const HistoryBanner = () => {
+    return (
+        <>
+            <div className="historyBanner">
+                <div className="sectionTitle">
+                </div>
+            </div>
+            <div className="bla"></div>
+        </>
+    );
+}
+
+export default HistoryBanner;
