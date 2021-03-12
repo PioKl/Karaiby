48a872235c468b5cbe761969776c47ec03f8b608
PioKl <kosiu342@gmail.com>
2021-03-12 20:28:37 +0100

    App.js|Add new component HistoryBanner

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import '../style/App.scss';
 import Header from './Header';
+import HistoryBanner from './HistoryBanner';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <HistoryBanner />
       </main>
       <footer>
 
