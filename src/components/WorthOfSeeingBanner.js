3c217cde3632e2a2c5c5f677f38909c26743326e
PioKl <kosiu342@gmail.com>
2021-03-17 20:20:37 +0100

    WorthOfSeeingBanner.js|Add new component WorthOfSeeingBanner

---
+++ src/components/WorthOfSeeingBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/WorthOfSeeingBanner.scss';
+
+const WorthOfSeeingBanner = () => {
+    return (
+        <>
+            <div className="worthOfSeeingBanner">
+                <div className="worthOfSeeingBanner__sectionTitle">
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default WorthOfSeeingBanner;
