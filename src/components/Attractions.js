a45a73bc6a3f23ed11acb5c5ae72830b158d6a7d
PioKl <kosiu342@gmail.com>
2021-03-31 20:40:14 +0200

    Attractions.js|Add Attractions component (testing)

---
+++ src/components/Attractions.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../style/Attractions.scss';
+
+const Attractions = () => {
+    return (
+        <>
+            <div className="testContainer">
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+                <div className="test test1">
+                    bla
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Attractions;
