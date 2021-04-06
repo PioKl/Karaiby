232d2f2a2d32d435864a18d2d370d93b7c58d6d8
PioKl <kosiu342@gmail.com>
2021-04-06 20:08:17 +0200

    FooterBanner.js|Add new component with functionality FooterBanner

---
+++ src/components/FooterBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/FooterBanner.scss';
+
+const FooterBanner = () => {
+    return (
+        <>
+            <div className="footerBanner">
+                <div className="footerBanner__sectionTitle">
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default FooterBanner;
