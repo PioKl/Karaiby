963712fe957e46dbe4c8e7c7e6cb0ac6e5d120a7
PioKl <kosiu342@gmail.com>
2021-04-15 20:11:16 +0200

    Add scroll into view and lock and unlock scroll functionality, new icon, working burger menu for mobile, removal of several classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/Header.scss';
 import bgVideo from '../images/headerBackground.mp4';
 import { ReactComponent as BurgerMenu } from '../svg/burgerMenu.svg';
-import { ReactComponent as MoveDown } from '../svg/moveDown.svg';
+import { ReactComponent as ClearIcon } from '../svg/clearIcon.svg';
 import { ReactComponent as MoveDownGold } from '../svg/trianglePolygonGold.svg';
 import { ReactComponent as MoveDownWhite } from '../svg/trianglePolygonWhite.svg';
+
+import { ReactComponent as Facebook } from '../svg/facebook.svg';
+import { ReactComponent as Instagram } from '../svg/instagram.svg';
+import { ReactComponent as Twitter } from '../svg/twitter.svg';
 const Header = () => {
+    const handleScroll = (e) => {
+        unlockScroll();
+        const scrollTo = document.getElementById(e.target.getAttribute('name'));
+        scrollTo.scrollIntoView({
+            block: `${e.target.getAttribute('name') === 'attractions' ? 'start' : 'center'}`,
+        });
+        setTimeout(() => {
+            setBurgerMenu(false);
+        }, 1000);
+    }
+    const [burgerMenu, setBurgerMenu] = useState(false)
+    const handleMenuBurger = () => {
+        setBurgerMenu(!burgerMenu);
+    }
+
+    //Zablokowanie scrolla, gdy otwiera sie menu, a nastepnie jego odblokowanie, gdy menu jest juz zamykane
+    const lockScroll = () => {
+        const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+        const body = document.body;
+        body.style.position = 'fixed';
+        body.style.top = `-${scrollY}`;
+    };
+    const unlockScroll = () => {
+        const body = document.body;
+        const scrollY = body.style.top;
+        body.style.position = '';
+        body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
+    window.addEventListener('scroll', () => {
+        document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+    });
+
     return (
         <header className="header">
 
@@ -14,17 +51,37 @@ const Header = () => {
                 <div className="logo">
                     <span className="logo__companyName">Travel</span>
                 </div>
-                <BurgerMenu className="navigation__burgerMenu" />
+                <ul className="menu">
+                    <li onClick={handleScroll} className="menu__item" name="history" title="Historia">Historia</li>
+                    <li onClick={handleScroll} className="menu__item" name="worthOfSeeing" title="Warto zobaczyć">Warto Zobaczyć</li>
+                    <li onClick={handleScroll} className="menu__item" name="attractions" title="Atrakcje">Atrakcje</li>
+                    <li onClick={handleScroll} className="menu__item" name="contact" title="Kontakt">Kontakt</li>
+                </ul>
+                <div className="social">
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><Instagram fill="white" className="social__icon social__icon--instagram" /></a>
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><Facebook fill="white" className="social__icon social__icon--facebook" /></a>
+                    <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><Twitter fill="white" className="social__icon social__icon--twitter" /></a>
+                </div>
+                {burgerMenu ? <ClearIcon onClick={() => { handleMenuBurger(); unlockScroll() }} className="navigation__clearIcon" /> : <BurgerMenu
+                    onClick={() => { handleMenuBurger(); lockScroll() }} className="navigation__burgerMenu" />}
+                <div className={`navigationContainer navigationContainer--${burgerMenu ? "visible" : "hidden"}`}>
+                    <ul className="navigationContainer__burgerItemList">
+                        <li onClick={handleScroll} name="mainSite" className="navigationContainer__burgerItem navigationContainer__burgerItem--mainSite">Strona Główna</li>
+                        <li onClick={handleScroll} name="history" className="navigationContainer__burgerItem">Historia</li>
+                        <li onClick={handleScroll} name="worthOfSeeing" className="navigationContainer__burgerItem">Warto Zobaczyć</li>
+                        <li onClick={handleScroll} name="attractions" className="navigationContainer__burgerItem">Atrakcje</li>
+                        <li onClick={handleScroll} name="contact" className="navigationContainer__burgerItem">Kontakt</li>
+                    </ul>
+                </div>
             </div>
 
             {/* Hero */}
-            <div className="hero">
+            <div id="mainSite" className="hero">
                 <div className="mainTitle">
                     <h1 className="mainTitle__firstTitle">Odkryj</h1>
                     <h1 className="mainTitle__secondTitle">Karaiby</h1>
                 </div>
-                <a href="#"><button className="hero__button">Sprawdź Atrakcje</button></a>
-                {/* <MoveDown className="hero__moveDown" /> */}
+                <button onClick={handleScroll} name="worthOfSeeing" className="hero__button">Sprawdź Miejsca</button>
                 <div className="hero__moveDown">
                     <MoveDownGold className="hero__triangle hero__triangle--gold" />
                     <MoveDownWhite className="hero__triangle hero__triangle--white" />
