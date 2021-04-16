4a6a2dd008614034f9115a36fec7334143ef0473
PioKl <kosiu342@gmail.com>
2021-04-16 14:23:04 +0200

    Add webp images and webp support

--- src/components/History.js
+++ src/components/History.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import '../style/History.scss';
 import map from '../images/caribbeanMap.png';
+import mapWebp from '../images/caribbeanMap.webp';
 import pirateStuff from '../images/pirateStuff.png';
+import pirateStuffWebp from '../images/pirateStuff.webp';
 const History = () => {
     return (
         <div id="history" className="history">
             <div className="history__content">
                 <div className="history__main">
-                    <img className="historyMap" src={map} alt="mapa karaibów" />
+                    <picture>
+                        <source srcSet={mapWebp} type="image/webp" />
+                        <source srcSet={map} type="image/png" />
+                        <img className="historyMap" src={mapWebp} alt="mapa karaibów" />
+                    </picture>
                     <div className="historyInfo">
                         <h1 className="historyInfo__title">O Karaibach</h1>
                         <p className="historyInfo__informations">Karaiby zostały odkryte w 1492 roku, przez Krzysztofa Kolumba. Następnie cały obszar został szybko skolonizowany przez cywilzacje zachodnie, stając się ważną częścią szlaków handlowych, przez co stały się miejscem częstego piractwa. Do końca XVII wieku z Tortugi wysyłano pirackie ekspedycje w celu oblegania i rabowania nadmorskich miast. Ważną siedzibą piratów był także Port Royal. Od XIX wieku kolonie stały się niepodległe.</p>
                     </div>
                 </div>
-                <img className="history__footer" src={pirateStuff} alt="pirackie rzeczy" />
+                <picture>
+                    <source srcSet={pirateStuffWebp} type="image/webp" />
+                    <source srcSet={pirateStuff} type="image/png" />
+                    <img className="history__footer" src={pirateStuffWebp} alt="pirackie rzeczy" />
+                </picture>
+
             </div>
 
         </div>
