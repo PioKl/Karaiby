5bd17e64667d9d32a34820ddbf3e3485e5518722
PioKl <kosiu342@gmail.com>
2021-04-16 14:19:52 +0200

    Add webp images for webp support

--- src/contexts/WorthOfSeeingContext.js
+++ src/contexts/WorthOfSeeingContext.js
@@ -3,7 +3,12 @@ import gallery1 from "../images/dominicaPhoto.png";
 import gallery2 from "../images/kubaPhoto.png";
 import gallery3 from '../images/pirateStuff.png';
 import gallery4 from "../images/jamajkaPhoto.png";
+import galleryWebp1 from "../images/dominicaPhoto.webp";
+import galleryWebp2 from "../images/kubaPhoto.webp";
+import galleryWebp3 from '../images/pirateStuff.webp';
+import galleryWebp4 from "../images/jamajkaPhoto.webp";
 import dominicaGallery1 from "../images/dominicaGallery1.png"; // do testów
+import dominicaGalleryWebp1 from "../images/dominicaGallery1.webp";
 
 export const WorthOfSeeingContext = createContext();
 
@@ -16,8 +21,10 @@ const WorthOfSeeingContextProvider = (props) => {
     /*     const gallery = [gallery1, gallery2, gallery3, gallery4];
         const placeName = ['Dominika', 'Kuba', 'Pirate', 'Jamajka']; */
     const gallery = [gallery1, gallery2, gallery4];
+    const galleryWebp = [galleryWebp1, galleryWebp2, galleryWebp4];
     const placeName = ['Dominika', 'Kuba', 'Jamajka'];
     const galleryOfPickedPlace = [[dominicaGallery1, gallery2, gallery3, gallery4], [gallery2, gallery3, gallery1, gallery4], [gallery4, gallery2, gallery3]]; //zdjecia testowe
+    const galleryOfPickedPlaceWebp = [[dominicaGalleryWebp1, galleryWebp2, galleryWebp3, galleryWebp4], [galleryWebp2, galleryWebp3, galleryWebp1, galleryWebp4], [galleryWebp4, galleryWebp2, galleryWebp3]];
 
     //const galleryOfPickedPlace = [[dominika1, dominika2, dominika3, dominika4], [kuba1,kuba2]]
     //src={galleryOfPickedPlace[imageCounter][imageCounterOfPickedPlace]}
@@ -56,7 +63,7 @@ const WorthOfSeeingContextProvider = (props) => {
     }
 
     return (
-        <WorthOfSeeingContext.Provider value={{ imageCounter, gallery, placeName, handlePreviousImage, handleNextImage, galleryOfPickedPlace, imageCounterOfPickedPlace, handlePreviousImageOfPickedPlace, handleNextImageOfPickedPlace }}>
+        <WorthOfSeeingContext.Provider value={{ imageCounter, gallery, galleryWebp, placeName, handlePreviousImage, handleNextImage, galleryOfPickedPlace, galleryOfPickedPlaceWebp, imageCounterOfPickedPlace, handlePreviousImageOfPickedPlace, handleNextImageOfPickedPlace }}>
             {props.children}
         </WorthOfSeeingContext.Provider>
     )
